6f15a51b76486e53e6e1c4937da36471acb02c21
EddyGonzalo <105296156+EddyGonzalo@users.noreply.github.com>
2023-05-10 11:49:26 -0500

    Update Crud.php

--- clases/Crud.php
+++ clases/Crud.php
@@ -3,19 +3,16 @@
     //require_once "./Conexion.php";
 
     class Crud extends Conexion{
-        public function songs()
-        {
-            try{
-                $conexion = parent::conectar();
-                $coleccion = $conexion->selectCollection('songs');
-                $datos = $coleccion->find();
-                return $datos;
-                $datos1 = $coleccion->find();
-                return $datos1;
-            }catch (\Throwable $th){
-                return $th->getMessage();
-            }
+    public function songs()
+    {
+        try{
+            $conexion = parent::conectar();
+            $coleccion = $conexion->selectCollection('songs');
+            $datos = $coleccion->find();
+            return $datos;
+        } catch (\Throwable $th){
+            return $th->getMessage();
         }
-
     }
+}
 ?>
